e6d2b962cbd08c2a807ef881557a0e454521c6c8
Salza <salzauh@gmail.com>
2025-02-12 20:25:13 +0100

    Add some pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/Habitats", "Habitats", "/pages/habitats.html"),
+  new Route("/habitats", "Nos Habitats", "/pages/habitats.html"),
+  new Route("/services", "Nos Services", "/pages/services.html"),
+  new Route("/contact", "Contact", "/pages/contact.html"),
+  new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
